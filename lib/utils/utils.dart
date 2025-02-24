a7ad46002b3a00418feb60c2074bedff5e231ad9
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-02-24 12:08:08 +0530

    first commit

---
+++ lib/utils/utils.dart
@@ -0,0 +1,23 @@
+class Utils {
+  static final List<String> gridText = [
+    "Recharge",
+    "Pay Bills",
+    "Landline",
+    "block Fiber",
+    "Updgrade to 4G",
+    "Choose your number",
+    "Do not disturb",
+    "Games",
+  ];
+
+  static final List<String> gridIcons = [
+    "Recharge",
+    "Pay Bills",
+    "Landline",
+    "block Fiber",
+    "Updgrade to 4G",
+    "Choose your number",
+    "Do not disturb",
+    "Games",
+  ];
+}
